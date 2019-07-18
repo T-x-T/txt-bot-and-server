9c86942c8a9ddc60da5682c88d2eeb6a3cf5f79d
TxT <contact@thetxt.club>
2019-07-18 20:01:46 +0200

    fix config-template.js

---
+++ config-example.js
@@ -0,0 +1,14 @@
+/*
+ *  EXAMPLE CONFIG FILE
+ *  Copy this file and rename to config.js, then customize all options
+ */
+
+
+//Create the container
+var conf = {
+    'http-port': 3000,
+    'https-port': 3001
+};
+
+//Export the container
+module.exports = conf;
