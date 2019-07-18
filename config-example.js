37c373fc2b82a59f81f4d38d88bd342610a13588
TxT <contact@thetxt.club>
2019-07-18 19:47:00 +0200

    Add config file including port numbers

---
+++ config-example.js
@@ -0,0 +1,14 @@
+/*
+ *  EXAMPLE CONFIG FILE
+ *  Copy this file and rename to config.js, then customize all options
+ */
+
+
+//Create the container
+var conf = {
+    'http-port': 'Enter the port as a plain number',
+    'https-port': 'Enter the port as a plain number'
+};
+
+//Export the container
+module.exports = conf;
