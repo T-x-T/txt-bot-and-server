05b4b9f55fabd34c0997ae6021f0c8745b0de6f8
TxT <contact@thetxt.club>
2019-07-29 19:55:02 +0200

    Almost done with drawing graphs, only the graph is missing

--- discord-bot/commands/admin.js
+++ discord-bot/commands/admin.js
@@ -2,6 +2,9 @@
  *	COMMAND FILE FOR ADMIN COMMANDS
  *	Command for all the admin thingys
  */
+
+//FIX: you need to send the command twice to get the up to date image
+
 const graph = require('./../../lib/graph.js');
 const fs = require('fs');
 const log = require('./../../lib/log.js');
@@ -16,7 +19,7 @@ module.exports = {
         //Performance graphs?
         if (args[0] == 'performance') {
             //Hand all the data to the graph file
-            graph.create(1920, 1080, 'Test-Title', 'TIME', 'FREE MEMORY', 1000, 500, 100, 100, [100, 200, 300, 400, 500, 600, 500, 400, 300, 200, 100, 0, 50, 1000, 1100], function (fileName) {
+            graph.create(1920, 1080, 'Test-Title', 'TIME', 'FREE MEMORY', 1000, 500, 150, 100, '-4d', '0%', 'Now', '100%', [100, 200, 300, 400, 500, 600, 500, 400, 300, 200, 100, 0, 50, 1000, 1100], function (fileName) {
                 var filePath = 'temp/' + fileName;
                 //Send the image
                 message.channel.send({
