41fe3fae0f4582e9d64554aa095ed8f7fa35b6f7
lukas <contact@thetxt.club>
2019-09-16 20:06:06 +0200

    Set period of performance log graphs to 7 days

--- web/handlers.js
+++ web/handlers.js
@@ -22,7 +22,7 @@ handlers.notFound = function (data, callback) {
 handlers.html = function (data, callback) {
   //Add path for different hosts
   if(data.headers.host.indexOf('thetxt.club') > -1) data.path = '/landing/' + data.path;
-  if(data.headers.host.indexOf('localhost') > -1) data.path = '/landing/' + data.path;
+  //if(data.headers.host.indexOf('localhost') > -1) data.path = '/landing/' + data.path;
     fs.readFile(path.join(__dirname, './html/' + data.path + '.html'), 'utf8', function (err, fileData) {
         if (!err && fileData.length > 0) {
             handlers.insertVariables(fileData, function(err, newFileData){
