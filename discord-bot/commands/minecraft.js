8f81c8a32a32d2e9704dde8893f4a102947c087c
lukas <contact@thetxt.club>
2019-10-19 13:24:40 +0200

    Added stats for top picked up items

--- discord-bot/commands/minecraft.js
+++ discord-bot/commands/minecraft.js
@@ -275,6 +275,21 @@ _internals.statsSwitch = function(collection, userID, ign, callback){
       callback(output);
     });
     break;
+    case 'top_picked_up':
+    mc_helpers.getStatTemplate(userID, 'topPickedUpItems', function(err, stats){
+      if(!err){
+        output += `Top picked up items from ${ign}:\n`;
+        let i = 0;
+        stats.forEach((entry) => {
+          output += `${i + 1}: ${stats[i].key}: ${stats[i].value}\n`
+          i++;
+        });
+      }else{
+        output += 'Something went wrong and I couldnt get the stats';
+      }
+      callback(output);
+    });
+    break;
     case 'top_mined':
     mc_helpers.getStatTemplate(userID, 'topMinedBlocks', function(err, stats){
       if(!err){
@@ -337,7 +352,7 @@ _internals.statsSwitch = function(collection, userID, ign, callback){
     });
     break;
     default:
-    output += 'I couldnt find that collection. Please use one of the following collecitons: general, distance, ores, total, top_usage, top_mined, top_killed, top_killed_by, total_per_death';
+    output += 'I couldnt find that collection. Please use one of the following collecitons: general, distance, ores, total, top_usage, top_picked_up, top_mined, top_killed, top_killed_by, total_per_death';
     callback(output);
     break;
   }
