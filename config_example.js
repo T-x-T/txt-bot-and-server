1eeaaab3a324360e6ecfc529eb34686ee4322e0f
t-x-t <contact@thetxt.club>
2020-04-28 13:49:52 +0200

    restructured config object, so each component has its own key

--- config_example.js
+++ config_example.js
@@ -3,53 +3,89 @@
  *  Copy this file and rename to config.js, then customize all options
  */
 
+let prod = {
+  application: {
 
-//Create the container
-var conf = {
-    'http-port': 3000,
-    'https-port': 3001,
-    'https-redirect': true, //enables automatic redirecting from http queries to https
-    'use-external-certs': true, //false for testing
-    'cert-path': '/etc/letsencrypt/live/{Domain}/',
-    'bot-prefix': '+',
-    'bot-token': 'ENTER YOUR TOKEN HERE',
-    'mongodb-url': 'mongodb://HOSTNAME/DATABASE',
-    'guild': 'guild id goes here as a number',
-    'admin-role': 'The role ID of the admin role goes here',
-    'paxterya-role': 'The role ID of the paxterya role goes here',
-    'log-level': 1,
-    'google-api-key': 'google api key here',
-    'youtube-video-announcements': [
-      {
-        'youtube_id': 'youtube channel id',
-        'channel_id': 'discord channel id',
-        'role': 'id of the role to ping'
-      }
-    ],
-    'mc-stats-remote': 'rclone remote name',
-    'rcon-server': '',
-    'rcon-port': 25575,
-    'rcon-password': '',
-    'mailUser': 'test@example.org',
-    'mailPass': 'badPW123',
-    'discord_client_id': '',
-    'discord_client_secret': '',
-    'discord_redirect_uri_application': '',
-    'discord_redirect_uri_staffLogin': '',
-    'new_member_announcement_channel': 'id of the channel new members get announced to',
-    'new_application_announcement_channel': '',
-    'new_bulletin_announcement_channel': '',
-    'general_channel': '',
-    'oauth_uris': {
-      'application': '',
-      'login': ''
-    },
-  'data_backend': 'mongo',
-  'bulletin': {
-    'max_per_usr': 5
   },
-  'db_upgrades':{
-    'mc_stats_sub_type': false
+  auth: {
+    discord_client_id: '',
+    discord_client_secret: '',
+    discord_redirect_uri_application: '',
+    discord_redirect_uri_staffLogin: '',
+    oauth_uris: {
+      application: '',
+      login: ''
+    }
+  },
+  bulletin: {
+    max_per_usr: 5
+  },
+  data: {
+    mongodb_url: 'mongodb://HOSTNAME/DATABASE',
+    data_backend: 'mongo',
+    db_upgrades:{
+      mc_stats_sub_type: false
+    }
+  },
+  discord_api: {
+
+  },
+  discord_bot: {
+    bot_prefix: '+',
+    bot_token: 'ENTER YOUR TOKEN HERE',
+    guild: 'guild id goes here',
+    channel: {
+      new_member_announcement: '',
+      new_application_announcement: '',
+      new_bulletin_announcement: '',
+      general
+    },
+    roles: {
+      admin: '',
+      paxterya: ''
+    }
+  },
+  email: {
+    mailUser: 'test@example.org',
+    mailPass: 'badPW123'
+  },
+  log: {
+    log_level: 1
+  },
+  minecraft: {
+    mc_stats_remote: 'rclone remote name',
+    rcon_server: '',
+    rcon_port: 25575,
+    rcon_password: ''
+  },
+  post: {
+
+  },
+  stats: {
+
+  },
+  user: {
+
+  },
+  web: {
+    http_port: 3000,
+    https_port: 3001,
+    https_redirect: true, //enables automatic redirecting from http queries to https
+    use_external_certs: true, //false to use internal certs that are not valid
+    cert_path: '/etc/letsencrypt/live/{Domain}/'
+  },
+  workers: {
+
+  },
+  youtube: {
+    google_api_key: 'google api key here',
+    youtube_video_announcements: [
+      {
+        youtube_id: 'youtube channel id',
+        channel_id: 'discord channel id',
+        role: 'id of the role to ping'
+      }
+    ]
   }
 };
 
