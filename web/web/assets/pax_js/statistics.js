df5c28810d84164e2422761eae094508556095ad
user.email <login@sevecke.me>
2020-05-13 18:26:10 +0200

    Fixed index.html and statistics.html

--- web/web/assets/pax_js/statistics.js
+++ web/web/assets/pax_js/statistics.js
@@ -1,35 +1,35 @@
 //Contains js for statistics.html
 
+window.onload = function () {
+    // scale worldmap correctly
+    
+    document.getElementById("map-container").children[0].setAttribute('viewBox', '0 50 900 400');
 
-// scale worldmap correctly
-document.addEventListener("onload",fix_worldmap());
-function fix_worldmap() {
-      document.getElementById("map-container").children[0].setAttribute('viewBox', '0 50 900 400');
+    // select first child of #map-container
+    var instance = new SVGPanZoom(document.getElementById("map-container").children[0], {
+        eventMagnet: document.getElementById('SVGContainer')
+    });
+
+    document.getElementById('zoomin').addEventListener('click', function () {
+        instance.zoomIn(null, 0.4);
+    });
+    document.getElementById('zoomout').addEventListener('click', function () {
+        instance.zoomOut(null, 0.4);
+    });
+    document.getElementById('resetSVG').addEventListener('click', function () {
+        instance.reset();
+    });
+    document.getElementById('panleft').addEventListener('click', function() {
+        instance.panLeft(200);
+    });
+    document.getElementById('panright').addEventListener('click', function() {
+        instance.panRight(200);
+    });
+    document.getElementById('panup').addEventListener('click', function() {
+        instance.panUp(200);
+    });
+    document.getElementById('pandown').addEventListener('click', function() {
+        instance.panDown(200);
+    });
+
 }
-
-  // select first child of #map-container
-  var instance = new SVGPanZoom(document.getElementById("map-container").children[0], {
-      eventMagnet: document.getElementById('SVGContainer')
-  });
-
-  document.getElementById('zoomin').addEventListener('click', function () {
-      instance.zoomIn(null, 0.4);
-  });
-  document.getElementById('zoomout').addEventListener('click', function () {
-      instance.zoomOut(null, 0.4);
-  });
-  document.getElementById('resetSVG').addEventListener('click', function () {
-      instance.reset();
-  });
-  document.getElementById('panleft').addEventListener('click', function() {
-      instance.panLeft(200);
-  });
-  document.getElementById('panright').addEventListener('click', function() {
-      instance.panRight(200);
-  });
-  document.getElementById('panup').addEventListener('click', function() {
-      instance.panUp(200);
-  });
-  document.getElementById('pandown').addEventListener('click', function() {
-      instance.panDown(200);
-  });
