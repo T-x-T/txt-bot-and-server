449aac2cea627b466021563ac78695a4239b1a92
t-x-t <contact@thetxt.club>
2020-06-05 11:15:52 +0200

    removed typing indicator in karma list command

--- src/discord_bot/commands/karma.js
+++ src/discord_bot/commands/karma.js
@@ -35,10 +35,8 @@ module.exports = {
         break;
       case 'list':
         //user wants to see the top all list
-        message.channel.startTyping();
         _internals.generateTopList(false, function(output){
-          message.channel.send(output, { split: true })
-            .then(message.channel.stopTyping(true));
+          message.channel.send(output, { split: true });
         });
         break;
       case 'rank':
