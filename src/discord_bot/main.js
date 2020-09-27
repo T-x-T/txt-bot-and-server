24ac72ba18290d6b0c0cc3f21de75284c5cf7c57
t-x-t <contact@thetxt.club>
2020-09-27 23:20:24 +0200

    removed bandaid fix for bug that got fixed in discordjs

--- src/discord_bot/main.js
+++ src/discord_bot/main.js
@@ -74,26 +74,6 @@ emitter.on('discord_bot_ready' ,() => {
     }
   });
 
-  //Gets called when something happens - this is necessary, so we can listen for reactions on non cached (old) messages
-  const events = {
-    MESSAGE_REACTION_ADD: 'messageReactionAdd',
-    MESSAGE_REACTION_REMOVE: 'messageReactionRemove'
-  };
-  client.on('raw', async event => {
-    if (!events.hasOwnProperty(event.t)) return;
-
-    const { d: data } = event;
-    const user = client.users.get(data.user_id);
-    const channel = client.channels.get(data.channel_id) || await user.createDM();
-
-    if (channel.messages.has(data.message_id)) return;
-
-    const message = await channel.fetchMessage(data.message_id);
-    const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
-    const reaction = message.reactions.get(emojiKey);
-    client.emit(events[event.t], reaction, user);
-    if (message.reactions.size === 1) message.reactions.delete(emojiKey);
-  });
 
   //Gets called when a reaction gets added to some message
   client.on('messageReactionAdd', (reaction, user) => {
