15641290a2753689c6cb808feb5232e0f03ae7f2
T-x-T <contact@thetxt.club>
2021-02-12 22:00:16 +0100

    Disable icon on next/previous post buttons if there are no more posts

--- nuxt_frontend/components/Blog.vue
+++ nuxt_frontend/components/Blog.vue
@@ -5,7 +5,7 @@
     <article>
       <div id="controls">
         <button id="prevPost" @click="prevPost()">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg v-if="currentId > 0" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
           </svg>
           Previous post
@@ -18,7 +18,7 @@
         </button>
         <button id="nextPost" @click="nextPost()">
           Next post
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg v-if="currentId < posts.length - 1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
           </svg>
         </button>
