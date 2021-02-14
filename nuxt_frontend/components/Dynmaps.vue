af90c9766af4aed71f98dccc73539af39924c5c3
T-x-T <contact@thetxt.club>
2021-02-14 11:47:53 +0100

    Make dynmap iframes active on click

--- nuxt_frontend/components/Dynmaps.vue
+++ nuxt_frontend/components/Dynmaps.vue
@@ -6,23 +6,33 @@
       <div class="item">
         <a href="https://paxterya.com/dynmap/survival" target="blank">
           <h1 class="link">Survival
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
           </svg></h1>
         </a>
-        <div class="overlay"></div>
+        <div class="overlay" @click="survivalActive = true" v-if="!survivalActive">
+          <svg class="pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
+          </svg>
+        </div>
         <iframe src="https://paxterya.com/dynmap/survival/?nocompass=true"></iframe>
+        <p @click="survivalActive = true" v-if="!survivalActive">To look around, click into the Map</p>
       </div>    
 
       <div class="item">
         <a href="https://paxterya.com/dynmap/creative" target="blank">
           <h1 class="link">Creative
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
           </svg></h1>
         </a>
-        <div class="overlay"></div>
+        <div class="overlay" @click="creativeActive = true" v-if="!creativeActive">
+          <svg class="pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
+          </svg>
+        </div>
         <iframe src="https://paxterya.com/dynmap/creative/?nocompass=true&mapname=surface"></iframe>
+        <p @click="creativeActive = true" v-if="!creativeActive">To look around, click into the Map</p>
       </div>   
 
     </div>
@@ -46,7 +56,7 @@ div#container
     width: 45vw
     h1.link:hover
       color: $pax-lightcyan
-    svg
+    svg.icon
       height: 35pt
       width: auto
       transform: translateY(5pt)
@@ -64,7 +74,34 @@ div#container
 
 div.overlay
   width: 45vw
-  height: 50vh
+  height: 45vh
   position: absolute
   z-index: 102
+  border-radius: 1vw
+  &:hover
+    box-shadow: 0px 0px 2vw #102f36
+    cursor: pointer
+  svg.pointer
+    color: white
+    width: 8vw
+    height: 8vw
+    position: absolute
+    top: 30vh
+    left: 37vw
+
+p
+  @extend .pax-p
+  color: white
+  text-align: center
+  cursor: pointer
+
 </style>
+
+<script>
+export default {
+  data: () => ({
+    survivalActive: false,
+    creativeActive: false,
+  })
+}
+</script>
