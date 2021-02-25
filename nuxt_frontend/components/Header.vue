4182a1b33df534bcdc86482b1aa0792430e443e6
T-x-T <contact@thetxt.club>
2021-02-25 19:40:15 +0100

    new translucent navbar

--- nuxt_frontend/components/Header.vue
+++ nuxt_frontend/components/Header.vue
@@ -1,32 +1,50 @@
 <template>
-  <div>
-    <NuxtLink to="/">Home</NuxtLink>
-    <NuxtLink to="/join-us">Apply</NuxtLink>
-    <NuxtLink to="/">Still Home</NuxtLink>
+  <div id="outer">
+    <div id="inner">
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/join-us">Apply</NuxtLink>
+      <NuxtLink to="/">Sumthin</NuxtLink>
+    </div>
   </div>
 </template>
 
 <style lang="sass" scoped>
 @import ~/assets/_vars.sass
 
-div
-  background: $pax-darkcyan
+div#outer
+  width: 100vw
+  display: flex
+  justify-content: center
+  position: fixed
+  top: 15px
+  z-index: 100
+  @media screen and ($mobile)
+    top: 0px
+
+div#inner
+  background: rgba(0, 0, 0, 0.25)
   display: flex
   flex-wrap: nowrap
   align-items: stretch
   justify-content: center
-  position: sticky
-  top: 0px
-  z-index: 100
-  box-shadow: 0px 0px 5px #102f36 inset
-  width: 100vw
-  height: 60px
+  width: max-content
+  backdrop-filter: blur(20px)
+  border-radius: 20px
+  padding-left: 40px
+  padding-right: 40px
   a
     color: white
     text-decoration: none
     @extend .pax-semibold
     font-size: 20pt
-    padding: 0.3% 3% 0.3% 3%
+    padding: 20px 40px 20px 40px
+    text-shadow: 0px 0px 5px #102f36
     &:hover
-      text-shadow: 0px 0px 10px $pax-lightcyan
+      text-shadow: 0px 0px 15px #102f36
+      @media screen and ($desktop)
+        transform: scale(1.2)
+  @media screen and ($mobile)
+    padding: 0
+    width: 100vw
+    border-radius: 0px
 </style>
