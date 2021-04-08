c724568668637b6695638e24c5f37a8af2c68b63
T-x-T <contact@thetxt.club>
2021-04-08 10:08:34 +0200

    automatically add unverified role to new people joining

--- src/discord_bot/main.js
+++ src/discord_bot/main.js
@@ -93,6 +93,10 @@ emitter.on('discord_bot_ready' ,() => {
 
   //Gets called whenever a new member joins the guild
   client.on('guildMemberAdd', (user) => {
+
+    //Add unverified role
+    discordHelpers.addMemberToRole(user.id, "829611102274322493");
+
     //Send a welcome message
     discordHelpers.sendMessage(`Welcome <@${user.id}>! If you are here for joining the Minecraft server, then please read the <#${user.guild.channels.find(channel => channel.name == "faq").id}> and read the rules at https://paxterya.com/rules. You can then apply under https://paxterya.com/join-us\nIf you have any questions just ask in <#${user.guild.channels.find(channel => channel.name == "support").id}>\nWe are looking forward to see you ingame :)`, config.discord_bot.channel.general, function (err) {
       if (err) global.log(2, 'discord_bot', 'discord_bot couldnt send the new application message', { err: err, application: doc });
