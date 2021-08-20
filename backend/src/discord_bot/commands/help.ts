e77229876632ba68789ef1da2b28a3a019ff3911
T-x-T <contact@thetxt.club>
2021-08-20 20:58:06 +0200

    adding link buttons to the help command

--- backend/src/discord_bot/commands/help.ts
+++ backend/src/discord_bot/commands/help.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, MessageActionRow, MessageButton } from "discord.js";
 import log = require("../../log/index.js");
 import minecraft = require("../../minecraft/index.js");
 
@@ -17,7 +17,6 @@ export = {
     }
 
     let output = "";
-    output += "**Join here:** <https://paxterya.com/join-us>\n";
     output += "**Survival Server IP:** paxterya.com\n";
     output += "**Creative Server IP:** creative.paxterya.com\n";
     output += `**Version:** ${minecraftVersion} java\n`;
@@ -25,6 +24,23 @@ export = {
     output += `**FAQ:** <#${interaction.guild.channels.cache.find(channel => channel.name == "faq").id}>\n`;
     output += "Check out all commands by typing /";
 
-    await interaction.reply(output);
+    const row = new MessageActionRow()
+                .addComponents(new MessageButton()
+                              .setLabel("Website")
+                              .setStyle("LINK")
+                              .setURL("https://paxterya.com")
+                )
+                .addComponents(new MessageButton()
+                              .setLabel("Apply here")
+                              .setStyle("LINK")
+                              .setURL("https://paxterya.com/join-us")
+                )
+                .addComponents(new MessageButton()
+                              .setLabel("Dynmap")
+                              .setStyle("LINK")
+                              .setURL("https://paxterya.com/dynmap/survival")
+                );
+
+    await interaction.reply({content: output, components: [row]});
   }
 }
